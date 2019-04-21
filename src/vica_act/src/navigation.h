3041487e4255b975b72ce92ea08c779f91365656
Halim DJERROUD <hdd@ai.univ-paris8.fr>
2019-04-21 21:30:41 +0200

    add goto position service

--- src/vica_act/src/navigation.h
+++ src/vica_act/src/navigation.h
@@ -7,9 +7,10 @@
  */
 
 #include "ros/ros.h"
+#include <tf/transform_datatypes.h>
+
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose.h>
-
 #include <vica_act/vica_act_goto_pos.h>
 
 
@@ -22,7 +23,11 @@ namespace vica_act {
 class navigation {
 public:
 
+
+	float distance_tolerance = 0.1;
+
 	nav_msgs::Odometry current_pose;
+
 	ros::NodeHandle *n;
 	ros::Publisher *movement_pub;
 
@@ -32,8 +37,25 @@ public:
 	bool env_goto_pos(vica_act::vica_act_goto_pos::Request &req, vica_act::vica_act_goto_pos::Response &res);
 
 	void get_odom_pose(const nav_msgs::Odometry::ConstPtr& msg);
+
+	float linear_velocity(geometry_msgs::Pose , float );
+
+	float angular_velocity(geometry_msgs::Pose , float );
+
+	float steering_angle(geometry_msgs::Pose goal_pose);
+
+	float euclidean_distance(geometry_msgs::Pose goal_pose);
+
+
+
 };
 
 } /* namespace vica_act */
 
 #endif /* VICA_ACT_SRC_NAVIGATION_H_ */
+
+
+
+
+
+
